cb655b6312e51cf2afe07694155aa58102ad9390
Iarlen <iarlensr@hotmail.com>
2023-05-03 20:03:53 -0300

    styles: adding responsiveness to the login page.

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import LoginImage from "/Images/Login.png";
 
@@ -44,7 +45,7 @@ const Login = () => {
                 <a href="#">
                   <FcGoogle />
                 </a>
-                <a href="#">Ainda não possui uma conta?</a>
+                <Link to="/register">Criar uma conta</Link>
               </LinkStyled>
             </ButtonContainer>
           </FormStyled>
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -4,6 +4,13 @@ export const LoginStyled = styled.section`
   width: 100%;
   display: grid;
   grid-template-columns: 1fr 1.5fr;
+
+  @media (max-width: 715px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: 0.5fr 1fr;
+
+    gap: ${({ theme }) => theme.gap.veryBig};
+  }
 `;
 
 export const InfoStyled = styled.div`
@@ -11,7 +18,7 @@ export const InfoStyled = styled.div`
   flex-direction: column;
 
   align-items: center;
-  gap: ${({ theme }) => theme.gap.veryBig};
+  gap: ${({ theme }) => theme.gap.regular};
 `;
 
 export const TitleContainer = styled.div`
@@ -36,20 +43,66 @@ export const TitleContainer = styled.div`
 
     color: ${({ theme }) => theme.colors.secundary};
   }
+
+  @media (max-width: 980px) {
+    p {
+      font-size: ${({ theme }) => theme.size.small};
+    }
+  }
+
+  @media (max-width: 875px) {
+    h1 {
+      font-size: ${({ theme }) => theme.size.big};
+    }
+  }
+
+  @media (max-width: 715px) {
+    text-align: center;
+    order: 3;
+
+    h1 {
+      font-size: ${({ theme }) => theme.size.veryBig};
+    }
+
+    p {
+      font-size: ${({ theme }) => theme.size.regular};
+    }
+  }
+
+  @media (max-width: 510px) {
+    h1 {
+      font-size: ${({ theme }) => theme.size.big};
+    }
+
+    p {
+      font-size: ${({ theme }) => theme.size.small};
+    }
+  }
 `;
 
 export const ImageStyled = styled.img`
   width: 325px;
   margin: 0 auto;
+
+  @media (max-width: 715px) {
+    width: 220px;
+  }
+
+  @media (max-width: 510px) {
+    width: 180px;
+  }
 `;
 
 export const FormContainer = styled.div`
-  max-width: 400px;
   display: flex;
   flex-direction: column;
   justify-content: center;
 
   margin: 0 auto;
+
+  @media (max-width: 715px) {
+    justify-content: start;
+  }
 `;
 
 export const FormStyled = styled.form`
@@ -80,6 +133,54 @@ export const FormStyled = styled.form`
 
     border: none;
     border-radius: 4px;
+  }
+
+  @media (max-width: 875px) {
+    label {
+      width: 350px;
+    }
+  }
+
+  @media (max-width: 770px) {
+    label {
+      width: 330px;
+    }
+  }
+
+  @media (max-width: 715px) {
+    label {
+      width: 450px;
+    }
+  }
+
+  @media (max-width: 510px) {
+    label {
+      width: 380px;
+    }
+  }
+
+  @media (max-width: 420px) {
+    label {
+      width: 340px;
+    }
+  }
+
+  @media (max-width: 380px) {
+    label {
+      width: 300px;
+    }
+  }
+
+  @media (max-width: 335px) {
+    label {
+      width: 270px;
+    }
+  }
+
+  @media (max-width: 310px) {
+    label {
+      width: 240px;
+    }
   }
 `;
 
