9c7d313ca50bf9fd92d41aca9d00d10ed26a0594
Iarlen <iarlensr@hotmail.com>
2023-05-06 22:43:06 -0300

    feat: adding validation to the registration form.

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -5,6 +5,12 @@ import { Link } from "react-router-dom";
 
 import RegisterImage from "/Images/Register.png";
 
+import { useForm } from "react-hook-form";
+
+import ErrorForm from "../../components/ErrorForm/ErrorForm";
+
+import * as EmailValidator from "email-validator";
+
 import {
   RegiterStyled,
   InfoStyled,
@@ -15,7 +21,27 @@ import {
   ButtonStyled,
 } from "./styles";
 
+interface IFormData {
+  name: string;
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<IFormData>();
+
+  const watchPassword = watch("password");
+
+  const onSubmit = (data: IFormData) => {
+    console.log(data);
+  };
+
   return (
     <ContainerStyled>
       <RegiterStyled>
@@ -27,22 +53,67 @@ const Register = () => {
           />
         </InfoStyled>
         <FormContainer>
-          <FormStyled>
-            <label htmlFor="">
+          <FormStyled onSubmit={handleSubmit(onSubmit)}>
+            <label>
               <span>Nome:</span>
-              <input type="text" placeholder="José Santos" />
+              <input
+                type="text"
+                placeholder="José Santos"
+                {...register("name", { required: true })}
+              />
+              {errors.name?.type === "required" && (
+                <ErrorForm message="O nome é obrigatório." />
+              )}
             </label>
             <label htmlFor="">
               <span>E-mail:</span>
-              <input type="email" placeholder="Email@gmail.com" />
+              <input
+                type="email"
+                placeholder="Email@gmail.com"
+                {...register("email", {
+                  required: true,
+                  validate: (value) => EmailValidator.validate(value),
+                })}
+              />
+              {errors.email?.type === "required" && (
+                <ErrorForm message="O E-mail é obrigatório." />
+              )}
+              {errors.email?.type === "validate" && (
+                <ErrorForm message="O E-mail é inválido." />
+              )}
             </label>
             <label htmlFor="">
               <span>Senha:</span>
-              <input type="password" />
+              <input
+                type="password"
+                {...register("password", { required: true, minLength: 8 })}
+              />
+              {errors.password?.type === "required" && (
+                <ErrorForm message="A senha é obrigtória." />
+              )}
+              {errors.password?.type === "minLength" && (
+                <ErrorForm message="A senha deve ser maior que 7 caracteres." />
+              )}
             </label>
             <label htmlFor="">
               <span>Confirmar Senha:</span>
-              <input type="password" />
+              <input
+                type="password"
+                {...register("passwordConfirmation", {
+                  required: true,
+                  minLength: 8,
+                  validate: (value) => value === watchPassword,
+                })}
+              />
+              {errors.passwordConfirmation?.type === "required" && (
+                <ErrorForm message="A senha é obrigtória." />
+              )}
+              {errors.passwordConfirmation?.type === "minLength" && (
+                <ErrorForm message="A senha deve ser maior que 7 caracteres." />
+              )}
+              {errors.passwordConfirmation?.type === "validate" && (
+                <ErrorForm message="As senhas não conferem." />
+              )}
             </label>
             <ButtonContainer>
               <ButtonStyled type="submit">Criar Conta</ButtonStyled>
