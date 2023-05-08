64acee0d55e6de54a08f2a942984a9dec46346cc
Iarlen <iarlensr@hotmail.com>
2023-05-08 00:54:04 -0300

    refactor: changing the way of creating routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { Navigate, BrowserRouter, Route, Routes } from "react-router-dom";
+
+import GlboalStyled from "./styles/Global";
+import { Theme } from "./styles/theme/theme";
+import { ThemeProvider } from "styled-components";
+
+import Header from "./components/Header/index";
+
+import { ModalProvider } from "./context/ModalContext";
+
+import { AuthProvider } from "./context/AuthContext";
+import Login from "./pages/Login/Login";
+import Home from "./pages/Home/Home";
+import Register from "./pages/Register/Register";
+import About from "./pages/About/About";
+
+// auth
+import { onAuthStateChanged, getAuth, User } from "firebase/auth";
+import { app } from "./services/firebase";
 
 function App() {
+  const auth = getAuth(app);
+  const [user, setUser] = useState<User | null>();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) return setUser(user);
+      setUser(null);
+    });
+  }, [auth, user]);
+
   return (
-    <div className="App">
-      <h1>Hello Word!</h1>
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <ModalProvider>
+          <ThemeProvider theme={Theme}>
+            <GlboalStyled />
+            <Header />
+            <Routes>
+              <Route
+                path="/"
+                element={user?.uid ? <Home /> : <Navigate to="/login" />}
+              />
+              <Route
+                path="/login"
+                element={!user?.uid ? <Login /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/register"
+                element={!user?.uid ? <Register /> : <Navigate to="/" />}
+              />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </ThemeProvider>
+        </ModalProvider>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
