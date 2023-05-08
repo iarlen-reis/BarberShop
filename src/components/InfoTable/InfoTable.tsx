45e88aa1c23acc32f376dab393bd615129d1077e
Iarlen <iarlensr@hotmail.com>
2023-05-08 20:35:18 -0300

    feat: displaying table only if user has one or more appointments.

--- src/components/InfoTable/InfoTable.tsx
+++ src/components/InfoTable/InfoTable.tsx
@@ -2,56 +2,57 @@ import React from "react";
 
 import { MdDelete } from "react-icons/md";
 import { BsSearch } from "react-icons/bs";
+import { IoAddOutline } from "react-icons/io5";
 
-import { TableStyled, TheadStyled, TBodyStyled } from "./styles";
+import { TableStyled, TheadStyled, TBodyStyled, NoFound } from "./styles";
+
+import { getUserDocuments } from "../../hooks/useFetchDocuments";
+import Loading from "../Loading/Loading";
 
 const InfoTable = () => {
+  const { schedules, loading } = getUserDocuments();
+
+  if (loading) return <Loading />;
+
   return (
-    <TableStyled>
-      <TheadStyled>
-        <tr>
-          <th>Serviço</th>
-          <th>Criado em</th>
-          <th>Horário agendado</th>
-          <th>Status</th>
-          <th>Ações</th>
-        </tr>
-      </TheadStyled>
-      <TBodyStyled>
-        <tr>
-          <td>Corte</td>
-          <td>05-05-2023</td>
-          <td>06-05-2023, 11:30</td>
-          <td>Pendente</td>
-          <td>
-            <BsSearch />
-            <MdDelete />
-          </td>
-        </tr>
-
-        <tr>
-          <td>Barba</td>
-          <td>05-05-2023</td>
-          <td>06-05-2023, 13:00</td>
-          <td>Cancelado</td>
-          <td>
-            <BsSearch />
-            <MdDelete />
-          </td>
-        </tr>
-
-        <tr>
-          <td>Barba</td>
-          <td>05-05-2023</td>
-          <td>06-05-2023, 13:00</td>
-          <td>Cancelado</td>
-          <td>
-            <BsSearch />
-            <MdDelete />
-          </td>
-        </tr>
-      </TBodyStyled>
-    </TableStyled>
+    <>
+      {schedules && schedules.length > 0 ? (
+        <TableStyled>
+          <TheadStyled>
+            <tr>
+              <th>Serviço</th>
+              <th>Criado em</th>
+              <th>Horário agendado</th>
+              <th>Status</th>
+              <th>Ações</th>
+            </tr>
+          </TheadStyled>
+          <TBodyStyled>
+            {schedules &&
+              schedules?.length > 0 &&
+              schedules.map((schedule) => (
+                <tr key={schedule.id}>
+                  <td>{schedule.service}</td>
+                  <td>{schedule.createdAt}</td>
+                  <td>{schedule.scheduledDate}</td>
+                  <td>{schedule.status}</td>
+                  <td>
+                    <BsSearch />
+                    <MdDelete />
+                  </td>
+                </tr>
+              ))}
+          </TBodyStyled>
+        </TableStyled>
+      ) : (
+        <NoFound>
+          <h2>Nenhum agendamento encontrado.</h2>
+          <p>
+            Clique em <IoAddOutline /> para agendar um horário.
+          </p>
+        </NoFound>
+      )}
+    </>
   );
 };
 
