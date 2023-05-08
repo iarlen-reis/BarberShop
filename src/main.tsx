8d7030e52bc8fc410f290588c609e1bbe1a43b25
Iarlen <iarlensr@hotmail.com>
2023-05-08 00:54:04 -0300

    refactor: changing the way of creating routes.

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,11 @@
 /* eslint-disable prettier/prettier */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
 
-import GlboalStyled from './styles/Global'
-import { Theme } from './styles/theme/theme'
-import { ThemeProvider } from 'styled-components'
-
-import { router } from './routes/routes'
-
-import { ModalProvider } from './context/ModalContext'
-
-import { AuthProvider } from './context/AuthContext'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <AuthProvider>
-        <ModalProvider>
-            <ThemeProvider theme={Theme}>
-                <GlboalStyled />
-                <RouterProvider router={router} />
-            </ThemeProvider>
-        </ModalProvider>
-    </AuthProvider>
+        <App />
   </React.StrictMode>
 )
