66b7204fb523a2becae5791eba817fdcfdd6809b
Iarlen <iarlensr@hotmail.com>
2023-05-08 23:35:36 -0300

    feat: creating functionality to pick up a specific service.

--- src/context/ModalContext.tsx
+++ src/context/ModalContext.tsx
@@ -1,31 +1,89 @@
-import React, { ReactNode, createContext, useContext, useState } from "react";
+import 
+React, 
+{ 
+ReactNode, 
+createContext, 
+useContext, 
+useEffect, 
+useState 
+} from "react";
+
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../services/firebase";
 
 const initalState = {
   isModalOpen: false,
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   setModal: () => {},
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  modalDetails: false,
+  // eslint-disable-next-line prettier/prettier
+  document: {} as IDocument,
+  useFetchDocument: () => ({}),
+  setModalDetails: () => ({})
 };
 
 interface IChildren {
   children: ReactNode;
 }
 
+interface IDocument {
+  id: string;
+  service: string;
+  description: string;
+  scheduledDate: string;
+  createdAt: string;
+}
+
 interface IModalContext {
   isModalOpen: boolean;
   setModal: () => void;
+  modalDetails: boolean;
+  document: IDocument;
+  useFetchDocument: (id: string) => void;
+  setModalDetails: (state: boolean) => void;
 }
+
+
 
 export const ModalContext = createContext<IModalContext>(initalState);
 
 export const ModalProvider = ({ children }: IChildren) => {
   const [isModalOpen, setIsModalOpen] = useState(initalState.isModalOpen);
+  const [modalDetails, setModalDetails] = useState(initalState.modalDetails);
+  const [document, setDocument] = useState<IDocument>(initalState.document);
 
   const setModal = () => {
     setIsModalOpen(!isModalOpen);
   };
 
+  const useFetchDocument = (id: string) => {
+      const getDocument = async () => {
+        try {
+          const refDoc = await doc(db, "schedules", id);
+          const docSnap = await getDoc(refDoc);
+  
+          // eslint-disable-next-line prettier/prettier
+          setDocument(docSnap.data() as IDocument);
+        } catch (error) {
+          console.log(error);
+        }
+        finally{
+            setModalDetails(true);
+        }
+      };
+      getDocument()
+  };
+
   return (
-    <ModalContext.Provider value={{ isModalOpen, setModal }}>
+    <ModalContext.Provider
+     value={{ 
+        isModalOpen, 
+        setModal, 
+        modalDetails, 
+        document, 
+        useFetchDocument, 
+        setModalDetails }}>
       {children}
     </ModalContext.Provider>
   );
