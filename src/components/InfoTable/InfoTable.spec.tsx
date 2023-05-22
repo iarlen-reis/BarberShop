e98e0a073f7d02b4208292593d073896c28c6c79
Iarlen <iarlensr@hotmail.com>
2023-05-21 21:20:28 -0300

    test: finalizing unit tests of the InfoTable component.

--- src/components/InfoTable/InfoTable.spec.tsx
+++ src/components/InfoTable/InfoTable.spec.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 
+// Browser, Theme and Provider
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { Theme } from "../../styles/theme/theme";
 
-import { AuthContext } from "../../context/AuthContext";
-import { mockAuthContextValue } from "../../__mocks__/UserMock";
-
+// Components
 import InfoTable from "./InfoTable";
+
+//Context
+import { deleteDocument } from "../../hooks/useDeleteDocument";
+import { ModalContext } from "../../context/ModalContext";
+import { AuthContext } from "../../context/AuthContext";
+
+// Vitest And React Library
 import { fireEvent, render } from "@testing-library/react";
+import { expect, vi } from "vitest";
 import "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import { expect, vi } from "vitest";
-import { deleteDocument } from "../../hooks/useDeleteDocument";
+
+// Mocks
+import { mockAuthContextValue } from "../../__mocks__/UserMock";
 
 vi.mock("../../hooks/useFetchDocuments", () => ({
   getUserDocuments: vi.fn(() => ({
@@ -31,55 +39,75 @@ vi.mock("../../hooks/useFetchDocuments", () => ({
         scheduledDate: "2023-05-21",
         status: "cancelado",
       },
+      {
+        id: 3,
+        service: "Serviço 3",
+        createdAt: "2023-05-16",
+        scheduledDate: "2023-05-21",
+        status: "finalizado",
+      },
     ],
     loading: false,
   })),
 }));
 
-const mockDeleteDocument = vi.fn();
-vi.mock("../../hooks/useDeleteDocument", () => ({
-  deleteDocument: mockDeleteDocument,
+export const mockUseFetchDocument = vi.fn();
+vi.mock("../../hooks/useFetchDocument", () => ({
+  useFetchDocument: () => ({
+    document: {},
+    getDocument: mockUseFetchDocument,
+  }),
 }));
 
-describe("Render InfoTable component", () => {
-  it("should render InfoTable component", () => {
-    render(
-      <BrowserRouter>
-        <AuthContext.Provider value={mockAuthContextValue}>
+const mockModalContextValue = {
+  isModalOpen: false,
+  setModal: vi.fn(),
+  filtered: "",
+  setFiltered: vi.fn(),
+  filter: false,
+  setFilter: vi.fn(),
+  modalDetails: false,
+  setModalDetails: vi.fn(),
+};
+
+const renderWithRouterThemeAndAuthentication = () => {
+  return render(
+    <BrowserRouter>
+      <AuthContext.Provider value={mockAuthContextValue}>
+        <ModalContext.Provider value={mockModalContextValue}>
           <ThemeProvider theme={Theme}>
             <InfoTable />
           </ThemeProvider>
-        </AuthContext.Provider>
-      </BrowserRouter>,
-    );
+        </ModalContext.Provider>
+      </AuthContext.Provider>
+    </BrowserRouter>,
+  );
+};
+
+//
+describe("InfoTable component", () => {
+  it("should render InfoTable component", () => {
+    renderWithRouterThemeAndAuthentication();
   });
 
   it("should display a pending service", () => {
-    const { getByText } = render(
-      <BrowserRouter>
-        <AuthContext.Provider value={mockAuthContextValue}>
-          <ThemeProvider theme={Theme}>
-            <InfoTable />
-          </ThemeProvider>
-        </AuthContext.Provider>
-      </BrowserRouter>,
-    );
+    const { getByText } = renderWithRouterThemeAndAuthentication();
 
     const servicePending = getByText("pendente");
 
     expect(servicePending).toBeInTheDocument();
   });
 
+  it("should display a finished service", () => {
+    const { getByText } = renderWithRouterThemeAndAuthentication();
+
+    const serviceCanceled = getByText("finalizado");
+
+    expect(serviceCanceled).toBeInTheDocument();
+  });
+
   it("should display a canceled service", () => {
-    const { getByText } = render(
-      <BrowserRouter>
-        <AuthContext.Provider value={mockAuthContextValue}>
-          <ThemeProvider theme={Theme}>
-            <InfoTable />
-          </ThemeProvider>
-        </AuthContext.Provider>
-      </BrowserRouter>,
-    );
+    const { getByText } = renderWithRouterThemeAndAuthentication();
 
     const serviceCanceled = getByText("cancelado");
 
@@ -91,19 +119,22 @@ describe("Render InfoTable component", () => {
       deleteDocument: vi.fn(),
     }));
 
-    const { getAllByTestId } = render(
-      <BrowserRouter>
-        <AuthContext.Provider value={mockAuthContextValue}>
-          <ThemeProvider theme={Theme}>
-            <InfoTable />
-          </ThemeProvider>
-        </AuthContext.Provider>
-      </BrowserRouter>,
-    );
+    const { getAllByTestId } = renderWithRouterThemeAndAuthentication();
+
     const deleteButton = getAllByTestId("buttonDelete");
 
     fireEvent.click(deleteButton[0]);
 
     expect(deleteDocument).toHaveBeenCalledWith(1);
+  });
+
+  it("should open Modal with the correct document ID", () => {
+    const { getAllByTestId } = renderWithRouterThemeAndAuthentication();
+
+    const buttonModal = getAllByTestId("buttonModal");
+
+    fireEvent.click(buttonModal[0]);
+
+    expect(mockUseFetchDocument).toBeCalledWith(1);
   });
 });
